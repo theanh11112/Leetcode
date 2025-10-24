345a7ee5528cd7e1630e7a102fdf07ea578c5cf4
luutheanh075-crypto <email_ca_bn@domain.com>
2025-10-24 10:58:58 +0700

    lesson 23 Merge K ListNode

--- leetcode_practice.py
+++ leetcode_practice.py
@@ -10,46 +10,75 @@ Hướng dẫn:
 """
 
 import numpy as np
-
-
+import heapq
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution(object):
-    def isValid(self, s):
+    def mergeKLists(self, lists):
         """
-        :type s: str
-        :rtype: bool
+        :type lists: List[Optional[ListNode]]
+        :rtype: Optional[ListNode]
         """
-        stack = [] 
-        mapping = { ")": "(", "}": "{", "]": "[" }
-        for char in s:
-            if char in mapping:
-                top_element = stack.pop() if stack else '#'
-                if mapping[char] != top_element:
-                    return False
-            else :
-                stack.append(char)
-        return not stack 
+        heap = [] 
 
-#     if char in mapping: la so sanh theo key chu khong phai so sanh theo value 
-#    mapping = { ")": "(", "}": "{", "]": "[" }  tao 1 dict de luu tru key va value           
-         
-   
+        for i, node in enumerate(lists):
+            if node:
+                heapq.heappush(heap, (node.val, i, node))
+
+        dummy = ListNode(0)
+        current = dummy
+
+        while heap:
+            val, i , node = heapq.heappop(heap)
+            current.next = ListNode(val)
+            current = current.next 
+            if node.next:
+                heapq.heappush(heap, (node.next.val, i, node.next))
+        return dummy.next 
+    
+# thay vi phai quan ly K con tro, ta them chi so i vao heap de tranh viec so sanh giua cac ListNode khong hop le
+# heap se luu vao 1 tupple (node.val, i, node) de dam bao tinh duy nhat khi so sanh 
+# khi pop ra thi lay duoc ca chi so i de biet duoc node thuoc list nao, va de tranh truong hop loi khi cac node co gia tri giong nhau ma khong co i de so sanh se sinh ra loi 
+     
+    
+    
 
 if __name__ == "__main__":
 
     # Tạo object 
     solution = Solution()   
-    # Test case 1  
-    s1 = "()"
-    print("Test case 1: ", solution.isValid(s1))  # Kết quả đúng: True            
-
-    # Test case 2   
-    s2 = "()[]{}"
-    print("Test case 2: ", solution.isValid(s2))  # Kết quả đúng: True  
-    # Test case 3   
-    s3 = "(]"
-    print("Test case 3: ", solution.isValid(s3))  # Kết quả đúng: False  
-    # Test case 4   
-    s4 = "([)]"             
-    print("Test case 4: ", solution.isValid(s4))  # Kết quả đúng: False 
-   
+    # Test case 1
+    list1 = ListNode(1, ListNode(4, ListNode(5)))   
+    list2 = ListNode(1, ListNode(3, ListNode(4)))
+    list3 = ListNode(2, ListNode(6))
+    lists = [list1, list2, list3]
+    merged_head = solution.mergeKLists(lists)
+    # In kết quả
+    current = merged_head
+    while current:
+        print(current.val, end=" -> ")
+        current = current.next
+    print("None")
+    # Test case 2
+    lists = []
+    merged_head = solution.mergeKLists(lists)
+    print(merged_head)  # Expected output: None
+    # Test case 3
+    lists = [None]
+    merged_head = solution.mergeKLists(lists)
+    print(merged_head)  # Expected output: None
+    # Thêm các test case khác nếu cần
+    # Test case 4
+    list1 = ListNode(2)
+    lists = [list1]
+    merged_head = solution.mergeKLists(lists)
+    current = merged_head
+    while current:
+        print(current.val, end=" -> ")
+        current = current.next
+    print("None")
+    
+ 
